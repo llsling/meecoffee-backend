d838ebd810d8224a48c860a9fb0f979cdc4915f3
llsling <aazz55233@gmail.com>
2025-06-03 01:27:33 +0800

    測試能否連線0603

--- routes/api.js
+++ routes/api.js
@@ -1,39 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const dns = require("dns").promises;
 const { Pool } = require("pg");
 
-let pool;
-// 資料庫連線
-(async () => {
-  try {
-    const resolved = await dns.lookup("db.rgypfenexbzbgzctfgsg.supabase.co", {
-      family: 4,
-    });
-
-    const { Pool } = require("pg");
-    const pool = new Pool({
-      connectionString:
-        "postgresql://postgres.rgypfenexbzbgzctfgsg:yMCsV76xe2g@+wt@aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres",
-      ssl: { rejectUnauthorized: false },
-      statement_timeout: 10000,
-    });
-    module.exports = pool;
-
-    // pool = new Pool({
-    //   host: resolved.address,
-    //   port: 5432,
-    //   user: "postgres",
-    //   password: "yMCsV76xe2g@+wt",
-    //   database: "postgre",
-    //   ssl: { rejectUnauthorized: false },
-    //   statement_timeout: 10000,
-    // });
-    console.log("PostgreSQL pool created");
-  } catch (err) {
-    console.error("Failed to resolve Supabase host:", err);
-  }
-})();
+const pool = new Pool({
+  connectionString:
+    "postgresql://postgres.rgypfenexbzbgzctfgsg:yMCsV76xe2g@+wt@aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres",
+  ssl: { rejectUnauthorized: false },
+  statement_timeout: 10000,
+});
+console.log("PostgreSQL pool created");
 
 // 測試用
 router.get("/", (req, res) => {
@@ -42,7 +17,6 @@ router.get("/", (req, res) => {
 
 // 商品 API
 router.get("/products", async (req, res) => {
-  if (!pool) return res.status(500).json({ error: "Database not connected" });
   const query = `
     SELECT 
       p.id, p.name, p.price, p.description, p.image_url AS img, p.country,p.kind_id,
@@ -65,8 +39,7 @@ router.get("/products", async (req, res) => {
 
 //商品分類
 router.get("/kinds", async (req, res) => {
-  if (!pool) return res.status(500).json({ error: "Database not connected" });
-  const query = "SELECT id,name FROM kind";
+  const query = "SELECT id, name FROM kind";
   try {
     const result = await pool.query(query);
     res.json(result.rows);
